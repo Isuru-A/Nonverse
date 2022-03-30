ba543ef8a72ae1dc3b6fc0125e602d895ba81cfe
isuru <isuru2003a@gmail.com>
2022-03-30 18:27:06 +1100

    Import js scripts

---
+++ src/scripts/api/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+let url = `http://${process.env.REACT_APP_API_SERVER}/`
+let token = ''
+
+class api_base {
+
+    async initialise() {
+        await axios.get(
+            `${url}sanctum/csrf-cookie`
+        )
+            .then(() => {
+                token = document.cookie.split("; ")
+                    .find(cookie => cookie.startsWith("XSRF-TOKEN="))
+                    .split("=")[1]
+            })
+        //console.log(`2: ${token}`)
+    }
+}
+
+export const api = axios.create({
+    baseURL: url,
+    headers: {
+        Accept: 'application/json',
+        'X-XSRF-TOKEN': token
+    }
+})
+
+export default new api_base()
